863eb5c5d5f8ee4a737aa268c8adfbac9c5a98fd
Santoshn26 <santosh.nc26@gmail.com>
2024-12-03 01:16:13 +0530

    Updated Mysql Port to an integer

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', '192.168.29.142')
-    MYSQL_PORT = os.getenv('MYSQL_PORT', '3306')
+    MYSQL_PORT = os.getenv('MYSQL_PORT', 3306)
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'Mrdev@0612')
     MYSQL_DB = os.getenv('MYSQL_DB', 'task_manager')
